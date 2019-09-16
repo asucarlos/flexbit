406c4cb8fcbb1254225d393c5a86c277cfbf3e8c
Melody Yun Chen <Melodyyunc@gmail.com>
2019-09-15 23:01:49 -0400

    Fit-002 adding exercise routes

---
+++ api/routes/exercise/exerciseRoutes.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const express = require(express);
+const router = express();
+const { listExercise, createExercise } = require("./exerciseService");
+
+// GET /exercise
+router.route("/exercise").get(async (req, res, next) => {
+  try {
+    const exercises = await listExercises();
+    res.status(200).send({
+      data: exercises
+    });
+  } catch (e) {
+    console.error(`An error has occured: ${e}`);
+    next(e);
+  }
+});
+
+// POST /exercise - after MVP
+router.route("/exercise").post(async (req, res, next) => {
+  const { exerciseData } = req.body;
+  try {
+    const exercise = await createExercise(exerciseData);
+    res.status(201).send({
+      data: [exercise]
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+// DELETE /exercise - after MVP
+
+exports.router = router;
