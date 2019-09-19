dae7024daf5d05415cc0c8448a6a15040f7d7a9f
Clint <l.clint91@gmail.com>
2019-09-18 21:00:15 -0400

    commented out comparePassword function

--- api/routes/users/userService.js
+++ api/routes/users/userService.js
@@ -13,7 +13,8 @@ exports.isUser = async ({ email, password }) => {
   try {
     const [user] = await User.find({ email });
     if (user) {
-      const match = await user.comparePassword(password);
+      // REMEMBER to add this function later. 
+      // const match = await user.comparePassword(password);
       if (match) {
         return user;
       }
