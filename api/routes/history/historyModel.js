ef516d34a0d409a29acc7541732187b2dce42932
Melody Yun Chen <Melodyyunc@gmail.com>
2019-09-22 15:44:08 -0400

    Fit-017 - adding history routes and updating ids to ref specific schemas

--- api/routes/history/historyModel.js
+++ api/routes/history/historyModel.js
@@ -2,15 +2,32 @@
 
 const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
+const sessionRef = require("../sessions/sessionModel");
 
 const historySchema = new Schema({
-  sessionId: {
+  _sessionId: {
     type: Schema.Types.ObjectId,
+    ref: sessionRef,
     required: true
   },
-  Date: {
-    type: Date
+  startTime: {
+    type: Date, // year-month-date
+    required: true
+  },
+  endTime: {
+    type: Date, //2019-05-01
+    required: true
   }
 });
+
+/*
+{
+	"data" : {
+		"_sessionId": "5d82bbb8f5077f486fd6bddc",
+		"startTime": "Sun Sep 22 2019 15:04:44 GMT-0400 (Eastern Daylight Time)",
+		"endTime" :"Sun Sep 22 2019 15:04:44 GMT-0400 (Eastern Daylight Time)"
+	}
+}
+*/
 
 module.exports = mongoose.model("history", historySchema, "history");
