96ce9f396e6ccec189036e038ab98a457d56a293
Clint <l.clint91@gmail.com>
2019-09-22 12:27:02 -0400

    Fit-019 changed express() to express.Router() in exerciseRoutes.js and categoryRoutes.js

--- api/routes/categories/categoryRoutes.js
+++ api/routes/categories/categoryRoutes.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const express = require("express");
-const router = express();
+const router = express.Router();
 const { listCategory, createCategory } = require("./categoryService");
 
 // GET Categories
