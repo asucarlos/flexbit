8fd098838eb624c994836469a449372eeec420c4
Melody Yun Chen <Melodyyunc@gmail.com>
2019-09-29 16:00:30 -0400

    Fit-011, added user roles currently not being used adding it for future implimentation

--- api/middleware/common.js
+++ api/middleware/common.js
@@ -4,3 +4,12 @@ exports.handleBodyRequestParsing = router => {
   router.use(bodyParser.urlencoded({ extended: true }));
   router.use(bodyParser.json());
 };
+
+// currently not using this
+exports.requireEditor = (req, res, next) => {
+  if (req.user.role !== "owner") {
+    res.status(401).send("Must be an editor to create posts");
+  } else {
+    next();
+  }
+};
