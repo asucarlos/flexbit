606a3382a2df911f7dece2b095e34faae4ac1d81
Melody Yun Chen <Melodyyunc@gmail.com>
2019-09-30 21:35:58 -0400

    Fit-026 adding session template

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,34 @@
 import React, { Component } from "react";
-import giphy from "../../assets/giphy.gif"
+import giphy from "../../assets/giphy.gif";
 import { Image } from "./styles";
-
+import Session from "../Session/Session";
 
 class Homepage extends Component {
   constructor() {
     super();
     this.state = {
-      user: false
-    }
+      user: true
+    };
   }
+
   welcomeSection = (
     <div className="notification">
       <h1 className="is-size-1 has-text-centered">Welcome to Flexbit!</h1>
-      <h3 className="is-size-3 has-text-centered">Your personal workout tracker login to get started!</h3>
+      <h3 className="is-size-3 has-text-centered">
+        Your personal workout tracker login to get started!
+      </h3>
       <div className="columns is-vcentered .is-centered">
         <figure className="column">
-          <Image className="image" src={giphy} alt="Veggies working out"/>
+          <Image className="image" src={giphy} alt="Veggies working out" />
         </figure>
       </div>
-    </div>);
+    </div>
+  );
 
   render(props) {
     return (
       <div className="container">
-        {this.state.user ? this.welcomeSection : this.welcomeSection}
+        {this.state.user ? <Session /> : this.welcomeSection}
       </div>
     );
   }
