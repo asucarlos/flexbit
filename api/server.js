8ee016d9b98b97ebb565c78fe4186ef131766dd0
Melody Yun Chen <Melodyyunc@gmail.com>
2019-10-06 14:05:45 -0400

    updating server.js

--- api/server.js
+++ api/server.js
@@ -11,10 +11,11 @@ const router = express();
 // const { router: bookRoutes } = require("./routes/books/bookRoutes");
 const { router: exerciseRoutes } = require("./routes/exercises/exerciseRoutes");
 const { router: userRoutes } = require("./routes/users/userRoutes");
-const { router: categoryRoutes } = require("./routes/categories/categoryRoutes");
+const {
+  router: categoryRoutes
+} = require("./routes/categories/categoryRoutes");
 const { router: historyRoutes } = require("./routes/history/historyRoutes");
-const { router: sessionRoutes } = require('./routes/sessions/sessionRoutes');
-
+const { router: sessionRoutes } = require("./routes/sessions/sessionRoutes");
 
 // 3. Require conatants
 const { URL, PORT } = require("./utils/constants");
@@ -26,12 +27,17 @@ const { applyMiddleware } = require("./utils");
 const middleWare = require("./middleware");
 applyMiddleware(middleWare, router);
 
+router.use("/", express.static("public"));
 // 5. Utilise routes
 router.use("/api/exercises", exerciseRoutes);
 router.use("/api/users", userRoutes);
 router.use("/api/categories", categoryRoutes);
 router.use("/api/history", historyRoutes);
-router.use('/api/sessions', sessionRoutes);
+router.use("/api/sessions", sessionRoutes);
+
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 // 6. Create a server from express instance
 const server = http.createServer(router);
