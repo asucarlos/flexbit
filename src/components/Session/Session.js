21052f48fe30fc450735982b2df6d64dcf758a3b
Melody Yun Chen <Melodyyunc@gmail.com>
2019-10-06 13:12:25 -0400

    Fix for importing calculator into session and modifying package.json for heroku deploy

--- src/components/Session/Session.js
+++ src/components/Session/Session.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Exercise from "../Exercise";
 import StartBtn from "../StartBtn";
+import Calculator from "../Calculator";
 
-const Session = ({ exerciseList}) => {
+const Session = ({ exerciseList }) => {
   const exerciseEl = exerciseList => {
     return exerciseList.map((exercise, i) => {
       return <Exercise key={i} exercise={exercise} canDelete={true} />;
@@ -12,7 +13,7 @@ const Session = ({ exerciseList}) => {
   return (
     <section className="box">
       <h1 className="title is-1">Today's Workout</h1>
-      <Calculator exerciseList={exerciseList}/>
+      <Calculator exerciseList={exerciseList} />
       <ul>{exerciseEl(exerciseList)}</ul>
       <StartBtn exerciseList={exerciseList} />
     </section>
